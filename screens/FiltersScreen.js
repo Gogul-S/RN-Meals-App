e6b5710d010ffc0594bd93a3ddb203e7cf84ea4c
Gogul Bharathi <gogulbharathi@ninjacart.com>
2020-03-17 13:01:57 +0530

    FilterScreen Changes

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,15 +1,61 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import ActionBarButton from '../components/ActionBarButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+const FilterSwitch = (props) => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text> {props.label} </Text>
+            <Switch
+                trackColor={{
+                    true: 'green'
+                }}
+                thumbColor={
+                    Platform.select({
+                        ios: 'white',
+                        android: 'green'
+                    })
+                }
+                value={props.value}
+                onValueChange={props.onValueChange} />
+        </View>
+    )
+}
+
 
 const FilterScreen = (props) => {
+
+    const { navigation } = props;
+    const [vegan, setVegan] = useState(false);
+    const [sugarFree, setSugarFree] = useState(false);
+    const [lactoseFree, setLactoseFree] = useState(false);
+    const [vegetarian, setVegetarian] = useState(false);
+
+    const saveFilter = useCallback(() => {
+        const saveFilter = {
+            vegan: vegan,
+            sugarFree: sugarFree,
+            lactoseFree: lactoseFree,
+            vegetarian: vegetarian
+        }
+
+        console.log(saveFilter);
+    }, [vegetarian, vegan, lactoseFree, sugarFree,])
+
+    useEffect(() => {
+        navigation.setParams({ save: saveFilter })
+    }, [saveFilter])
+
     return (
         <View style={styles.screen}>
-            <Text>
-                FilterScreen
+            <Text style={styles.title}>
+                Available Filters
             </Text>
+            <FilterSwitch label='Sugar-free' value={sugarFree} onValueChange={(newValue) => { setSugarFree(newValue) }} />
+            <FilterSwitch label='Vegan' value={vegan} onValueChange={(newValue) => { setVegan(newValue) }} />
+            <FilterSwitch label='Lacotose-free' value={lactoseFree} onValueChange={(newValue) => { setLactoseFree(newValue) }} />
+            <FilterSwitch label='Vegetarian' value={vegetarian} onValueChange={(newValue) => { setVegetarian(newValue) }} />
         </View>
     )
 }
@@ -19,9 +65,18 @@ FilterScreen.navigationOptions = (navigationData) => {
         headerLeft: () => {
             return (
                 <HeaderButtons HeaderButtonComponent={ActionBarButton}>
-                    <Item title="Menu" iconName='ios-menu' onPress = { () => {
+                    <Item title="Menu" iconName='ios-menu' onPress={() => {
                         navigationData.navigation.toggleDrawer()
-                    }}/>
+                    }} />
+                </HeaderButtons>
+            )
+        },
+        headerRight: () => {
+            return (
+                <HeaderButtons HeaderButtonComponent={ActionBarButton}>
+                    <Item title="Save" iconName='ios-save' onPress={
+                        navigationData.navigation.getParam('save')
+                    } />
                 </HeaderButtons>
             )
         }
@@ -31,8 +86,20 @@ FilterScreen.navigationOptions = (navigationData) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '80%',
+        margin: 10
     }
 })
 
